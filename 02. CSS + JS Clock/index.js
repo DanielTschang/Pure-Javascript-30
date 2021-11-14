5978980ac74df18d91ad3ae2a8b0323630fc6be1
danchang11 <danchang11@gapp.nthu.edu.tw>
2021-11-14 18:06:27 +0800

    day 2

---
+++ 02. CSS + JS Clock/index.js
@@ -0,0 +1,21 @@
+const SecondHand = document.querySelector('.second-hand')
+const MinHand = document.querySelector('.min-hand')
+const HourHand = document.querySelector('.hour-hand')
+
+const setDate = () =>{
+  const now = new Date();
+  const sec = now.getSeconds();
+  const secDegree = (sec * 6) + 90;
+  SecondHand.style.transform = `rotate(${secDegree}deg)`
+
+  const min = now.getMinutes();
+  const minDegree = (min * 6) + 90
+  MinHand.style.transform = `rotate(${minDegree}deg)`
+  
+  const hour = now.getHours();
+  let hour_ = hour % 12
+  const hourDegree = (hour_ * 30) + 90
+  HourHand.style.transform = `rotate(${hourDegree}deg)`
+}
+
+setInterval(setDate,1000)   
